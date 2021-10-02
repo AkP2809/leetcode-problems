ab603fb702acac750f1dd135c4d8679c7d6a62b2
VritikaMalhotra <vritikamalhotra23@gmail.com>
2021-10-02 22:14:34 +0530

    Reverse Integer code added in Python folder

---
+++ Python/ReverseInteger_VritikaMalhotra.py
@@ -0,0 +1,11 @@
+def reverse_integer(n):
+   y = str(abs(n))
+   y = y.strip()
+   y = y[::-1]
+   output = int(y)
+   if output >= 2** 31 -1 or output <= -2** 31:
+       return 0
+   elif n < 0:
+      return -1 * output
+   else:
+      return output
