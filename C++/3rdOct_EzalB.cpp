4bfd296580dbc7f630075782af53ee0f48690c52
Rusheel Dalal <59964395+EzalB@users.noreply.github.com>
2021-10-03 16:35:10 +0530

    Create 3rdOct_EzalB.cpp

---
+++ C++/3rdOct_EzalB.cpp
@@ -0,0 +1,13 @@
+int removeDuplicates(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        int j = 1;
+        for(int i = 1; i<nums.size();i++)
+            if(nums[i-1]!=nums[i])
+            {
+                nums[j] = nums[i];
+                j++;
+            }
+        nums.resize(j);
+        return j;   
+    }
