9ac5bba9147b7b5ebea1b49e095e12e73ae5ee07
Rishabh9067 <31346897+Rishabh9067@users.noreply.github.com>
2022-10-16 17:09:33 +0530

    Create MoveZeros.cpp

---
+++ MoveZeros.cpp
@@ -0,0 +1,39 @@
+/*
+Move Zeros
+
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+Note that you must do this in-place without making a copy of the array.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-231 <= nums[i] <= 231 - 1
+ 
+
+Follow up: Could you minimize the total number of operations done?
+*/
+
+ void moveZeroes(vector<int>& nums) {
+        int count=0;
+        int i=0;
+        while(i<nums.size()){
+            if(nums[i]!=0)
+                nums[count++]=nums[i];
+            i++;
+        }
+        while(count<nums.size())
+            nums[count++]=0;
+    }
